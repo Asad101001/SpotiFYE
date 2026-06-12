74f5a48dbd18be4c56f38a59aebbbf1fdcdf26e7
Asad <muhammadasadk42@gmail.com>
2026-06-13 03:43:19 +0500

    Clean up folders, generate app icon, refactor build process, and move branch to main

--- build.bat
+++ build.bat
@@ -1,20 +1,20 @@
 @echo off
-echo =====================================
-echo   SpotiFYE - DSA Music Player Build
-echo =====================================
+cls
 
-if not exist build mkdir build
+echo [1/3] Compiling application icon...
+windres assets\resource.rc -O coff -o assets\resource.res
 
-g++ src\main.cpp -o build\SpotiFYE.exe -I lib\include -L lib\lib -lraylib -lopengl32 -lgdi32 -lwinmm
+echo [2/3] Compiling SpotiFYE...
+g++ src\main.cpp assets\resource.res -o build\SpotiFYE.exe -I lib\include -L lib\lib -lraylib -lopengl32 -lgdi32 -lwinmm -static-libgcc -static-libstdc++
 
 if %errorlevel% neq 0 (
     echo.
-    echo [ERROR] Build failed! Check your code for syntax errors.
+    echo [!] Compilation failed!
     pause
     exit /b %errorlevel%
 )
 
-echo [SUCCESS] Build complete!
+echo [3/3] Compilation successful!
 echo.
 echo Launching SpotiFYE...
-build\SpotiFYE.exe
+start "" "build\SpotiFYE.exe"
